b3d637c23e253f8719d705b7a6964d638dbbb57a
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-06-16 17:00:10 -0400

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,19 @@
+$("p").hide();
+
+var name = prompt("Provide a launchers name?");
+var occupation = prompt("What was your worst job?");
+var animal = prompt("What is your favorite animal?");
+var object = prompt("Ruby or Python?");
+
+var launchersLeague = name + " was just an ordinary, " + occupation + ". Until one day when he/she met a magic, talking " + animal + " that gave them a " + object + " that changed " + name + "'s whole life.";
+
+// var recipeForSuccess = "Combine, " + skill + ", " + lesson + " and " + noun + " in a small pan on high heat. Let " + projectName + " simmer for a night uncovered. In a separate bowl, chop up and combine " + noun + " and " + sideProject + " stir into the " + projectName + " mixture very slowly and stir consistently until you get a nice thick " + language + " base. Then put into a " + framework + " and let it cool for 15 minutes. Add a sprinkle of " + word + "Serve either warm or chilled. Serves one, best with a side of brussels sprouts.";
+//
+var fresh_prince = "Now this is a story all about how my " + noun + " got flipped " + verb + " upside down. And I'd like to take a " + unit_of_time + " just sit right down and I'll tell you how I became the " + occupation + " of a place called Chinatown. In west " + hometown + "delphia born and raised, on the " + place + " was where I spent most of my days. Chillin out maxin relaxin all cool, and all " + hobby + " outside of the school, when a couple of " + animalPlural + " who were up to no good, started making trouble in my neighborhood I got in one game of " + boardgame + " and my momma frowned, She said: you\'re movin with 40 " + adjForLaunchers + " people in Chinatown. I " + verb +
+" for a cab and when it came near, the license plate said " + adjective + " and it had dice in the mirror. If anything I could say " + noun2 + " had soul, but I thought: Nah, forget it - Yo, home to Mission Control. I pulled up to 33 Harrison about 7 or 8. And I yelled to the cabbie Yo home " + verb + " ya later! I looked at my kingdom I was finally there to code on my throne  as the " + occupation + " of " Chinatown."
+
+// var launchersThemeSong =
+// var launchersLife = Burrito app, pair pals
+$(".launchersLeague").append("<p>" + launchersLeague + "</p>");
+
+$("p").show();
