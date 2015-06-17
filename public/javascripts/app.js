973087b35b8738acf249b36ff09a8947b1705472
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-06-17 18:41:30 -0400

    work in progress

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,56 @@
-$("p").hide();
+$(document).ready(function() {
+  $.ajax({
+    type: "GET",
+    url: "/.json",
+    dataType: "json",
+    success: function(data) {
+      alert("We win");
+      debugger;
+      stories
+      // stories.forEach(function(story) {
+      //   $("<p>" + story.content + "</p>").show;
+      // });
+  },
+    error: function() {
+      alert("in the error block");
+      // do something else
+    }
+  });
+});
 
-var name = prompt("Provide a launchers name?");
-var occupation = prompt("What was your worst job?");
-var animal = prompt("What is your favorite animal?");
-var object = prompt("Ruby or Python?");
+$("#new-story-button").click(function(e) {
+  e.preventDefault();
 
-var launchersLeague = name + " was just an ordinary, " + occupation + ". Until one day when he/she met a magic, talking " + animal + " that gave them a " + object + " that changed " + name + "'s whole life.";
+  $.ajax({
+    type: "POST",
+    url: "/",
+    data: {name: name, content: content, noun: noun, verb: verb, time: time, occupation: occupation, hometown: hometown, place: place, hobby: hobby, animal: animal, boardgame: boardgame, adjective: adjective, verb_two: verb_two, adjective_two: adjective_two, noun_two: noun_two, verb_three: verb_three},
+    dataType: "json",
+    success: function(story) {
 
-// var recipeForSuccess = "Combine, " + skill + ", " + lesson + " and " + noun + " in a small pan on high heat. Let " + projectName + " simmer for a night uncovered. In a separate bowl, chop up and combine " + noun + " and " + sideProject + " stir into the " + projectName + " mixture very slowly and stir consistently until you get a nice thick " + language + " base. Then put into a " + framework + " and let it cool for 15 minutes. Add a sprinkle of " + word + "Serve either warm or chilled. Serves one, best with a side of brussels sprouts.";
+
+
+    },
+    error: function() {
+      alert("Sorry something went wrong");
+    }
+  });
+});
+
+// $("p").hide();
+
 //
-var fresh_prince = "Now this is a story all about how my " + noun + " got flipped " + verb + " upside down. And I'd like to take a " + unit_of_time + " just sit right down and I'll tell you how I became the " + occupation + " of a place called Chinatown. In west " + hometown + "delphia born and raised, on the " + place + " was where I spent most of my days. Chillin out maxin relaxin all cool, and all " + hobby + " outside of the school, when a couple of " + animalPlural + " who were up to no good, started making trouble in my neighborhood I got in one game of " + boardgame + " and my momma frowned, She said: you\'re movin with 40 " + adjForLaunchers + " people in Chinatown. I " + verb +
-" for a cab and when it came near, the license plate said " + adjective + " and it had dice in the mirror. If anything I could say " + noun2 + " had soul, but I thought: Nah, forget it - Yo, home to Mission Control. I pulled up to 33 Harrison about 7 or 8. And I yelled to the cabbie Yo home " + verb + " ya later! I looked at my kingdom I was finally there to code on my throne  as the " + occupation + " of " Chinatown."
-
-// var launchersThemeSong =
-// var launchersLife = Burrito app, pair pals
-$(".launchersLeague").append("<p>" + launchersLeague + "</p>");
-
-$("p").show();
+// $(".launchersLeague").append("<p>" + launchersLeague + "</p>");
+//
+// $("p").show();
+//
+//
+//
+//
+//
+//
+// // var launchersLeague = name + " was just an ordinary, " + occupation + ". Until one day when he/she met a magic, talking " + animal + " that gave them a " + object + " that changed " + name + "'s whole life.";
+//
+// // var recipeForSuccess = "Combine, " + skill + ", " + lesson + " and " + noun + " in a small pan on high heat. Let " + projectName + " simmer for a night uncovered. In a separate bowl, chop up and combine " + noun + " and " + sideProject + " stir into the " + projectName + " mixture very slowly and stir consistently until you get a nice thick " + language + " base. Then put into a " + framework + " and let it cool for 15 minutes. Add a sprinkle of " + word + "Serve either warm or chilled. Serves one, best with a side of brussels sprouts.";
+// //
+// // var launchersLife = Burrito app, pair pals
